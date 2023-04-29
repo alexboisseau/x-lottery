58e49d4132421568361c3442a65deb79078a5a52
alexboisseau <alex.boisseau79@gmail.com>
2023-04-29 19:11:21 +0200

    feat: add a new property in the Lottery contract
    Add an entranceFee property in the Lottery contract which is immutable and must be defined during
    the deployment.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-contract Lottery {}
+contract Lottery {
+  uint256 private immutable i_entranceFee; // i for immutable
+
+  constructor(uint256 _entranceFee) {
+    i_entranceFee = _entranceFee;
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
+}
--- test/Lottery.spec.ts
+++ test/Lottery.spec.ts
@@ -3,15 +3,22 @@ import { Contract, ContractFactory } from "ethers";
 import { expect } from "chai";
 
 describe("Lottery", () => {
-  let LotteryContractFactory: ContractFactory | null = null;
-  let LotteryContract: Contract | null = null;
+  let LotteryContractFactory: ContractFactory;
+  let LotteryContract: Contract;
+  let entranceFee = 10;
 
   beforeEach(async () => {
     LotteryContractFactory = await hre.ethers.getContractFactory("Lottery");
-    LotteryContract = await LotteryContractFactory.deploy();
+    LotteryContract = await LotteryContractFactory.deploy(entranceFee);
   });
 
   it("should be deployed", () => {
     expect(LotteryContract).instanceOf(Contract);
+  });
+
+  it("entrance fee should be define and equal to the entranceFee variable value", async () => {
+    const result = await LotteryContract.getEntranceFee();
+
+    expect(result).equal(entranceFee);
   });
 });
