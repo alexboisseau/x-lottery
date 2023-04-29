1bcd77b969b66c121a6a66e82dab6b566a148fd6
alexboisseau <alex.boisseau79@gmail.com>
2023-04-29 20:05:24 +0200

    feat: add new methods to the Lottery contract
    Add enterLottery and getPlayers methods in the Lottery contract with associated unit tests.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
+error Lottery__NotEnoughETHEntered();
+
 contract Lottery {
+  /* State Variables */
   uint256 private immutable i_entranceFee; // i for immutable
+  address payable[] private s_players; // s for storage
 
   constructor(uint256 _entranceFee) {
     i_entranceFee = _entranceFee;
   }
 
+  function enterLottery() public payable {
+    if (msg.value < i_entranceFee) {
+      revert Lottery__NotEnoughETHEntered();
+    }
+
+    s_players.push(payable(msg.sender));
+  }
+
   function getEntranceFee() public view returns (uint256) {
     return i_entranceFee;
+  }
+
+  function getPlayers() public view returns (address payable[] memory) {
+    return s_players;
   }
 }
--- test/Lottery.spec.ts
+++ test/Lottery.spec.ts
@@ -1,11 +1,12 @@
 import hre from "hardhat"; // HRE means Hardhat Runtime Environment
 import { Contract, ContractFactory } from "ethers";
 import { expect } from "chai";
+import { parseEther } from "ethers/lib/utils";
 
 describe("Lottery", () => {
   let LotteryContractFactory: ContractFactory;
   let LotteryContract: Contract;
-  let entranceFee = 10;
+  let entranceFee = parseEther("0.1");
 
   beforeEach(async () => {
     LotteryContractFactory = await hre.ethers.getContractFactory("Lottery");
@@ -20,5 +21,25 @@ describe("Lottery", () => {
     const result = await LotteryContract.getEntranceFee();
 
     expect(result).equal(entranceFee);
+  });
+
+  it("should throw an error if msg.value is less than the entranceFee", async () => {
+    await expect(
+      LotteryContract.enterLottery({
+        value: parseEther("0.09"),
+      })
+    ).to.be.revertedWithCustomError(
+      LotteryContract,
+      "Lottery__NotEnoughETHEntered"
+    );
+  });
+
+  it("should push the player in the players array since the msg.value is equal to the entranceFee", async () => {
+    await LotteryContract.enterLottery({
+      value: entranceFee,
+    });
+    const players = await LotteryContract.getPlayers();
+
+    expect(players.length).equal(1);
   });
 });
