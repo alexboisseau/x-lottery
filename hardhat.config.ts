f8800b0fa80b099d0ed68b9278e64e8af030f3aa
alexboisseau <alex.boisseau79@gmail.com>
2023-05-01 09:04:26 +0200

    feat: add packages to deploy easily complied smart contracts on several networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,47 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
+
+import { HardhatUserConfig } from 'hardhat/config';
+
+// Specific to the Metamask account
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// Specific to the Sepolia network
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL || 'https://eth-sepolia.g.alchemy.com/v2/api-key';
+const SEPOLIA_CHAIN_ID = process.env.SEPOLIA_CHAIN_ID || '123';
+
+// Specific to Etherscan
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'abcde';
+
+// Specific tp Coinmarketcap
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'abcde';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  defaultNetwork: 'hardhat',
+  networks: {
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
   gasReporter: {
-    enabled: true,
-    currency: "USD",
+    enabled: false,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
