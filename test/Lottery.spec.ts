546dff777896332e732fc527990cd22b7e3bc0aa
alexboisseau <alex.boisseau79@gmail.com>
2023-05-01 10:59:12 +0200

    test: add lottery unit tests
    Create a suite of unit tests associated to the deployments to check that Lottery and
    VRFCoordinatorV2Mock has been deployed correctly.

--- test/Lottery.spec.ts
+++ test/Lottery.spec.ts
@@ -1,68 +1,29 @@
-import hre, { ethers, network } from 'hardhat'; // HRE means Hardhat Runtime Environment
-import { Contract, ContractFactory } from 'ethers';
+import { deployments, ethers, network } from 'hardhat';
+import { developmentChains } from '../helper-hardhat.config';
+import { Contract } from 'ethers';
 import { expect } from 'chai';
-import { parseEther } from 'ethers/lib/utils';
 
-describe('Lottery', () => {
-  let LotteryContractFactory: ContractFactory;
-  let LotteryContract: Contract;
-  let entranceFee = parseEther('0.1');
+if (developmentChains.includes(network.name)) {
+  describe('Lottery Unit Tests', async function () {
+    let lotteryContract: Contract;
+    let vrfCoordinatorV2MockContract: Contract;
 
-  /* VRF constants */
-  const coordinatorAddress = '0x360d3cb7a189e469f68391bf0cf169025505b5ff';
-  const gasLane =
-    '0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c';
-  const subscriptionId = 1618;
-  const callbackGasLimit = 10000000;
-
-  beforeEach(async () => {
-    const [player] = await ethers.getSigners();
-    console.log('PLAYER : ', player);
-    LotteryContractFactory = await hre.ethers.getContractFactory('Lottery');
-    LotteryContract = await LotteryContractFactory.deploy(
-      entranceFee,
-      coordinatorAddress,
-      gasLane,
-      subscriptionId,
-      callbackGasLimit
-    );
-  });
-
-  it('should be deployed', () => {
-    expect(LotteryContract).instanceOf(Contract);
-  });
-
-  it('entrance fee should be define and equal to the entranceFee variable value', async () => {
-    const result = await LotteryContract.getEntranceFee();
-
-    expect(result).equal(entranceFee);
-  });
-
-  it('should throw an error if msg.value is less than the entranceFee', async () => {
-    await expect(
-      LotteryContract.enterLottery({
-        value: parseEther('0.09'),
-      })
-    ).to.be.revertedWithCustomError(
-      LotteryContract,
-      'Lottery__NotEnoughETHEntered'
-    );
-  });
-
-  it('should push the player in the players array since the msg.value is equal to the entranceFee', async () => {
-    await LotteryContract.enterLottery({
-      value: entranceFee,
+    this.beforeEach(async function () {
+      await deployments.fixture('all'); // Deploy all contracts which have the tag 'all';
+      lotteryContract = await ethers.getContract('Lottery');
+      vrfCoordinatorV2MockContract = await ethers.getContract(
+        'VRFCoordinatorV2Mock'
+      );
     });
-    const players = await LotteryContract.getPlayers();
 
-    expect(players.length).equal(1);
-  });
+    describe('Deployments', function () {
+      it('Lottery should be deployed', function () {
+        expect(lotteryContract).instanceOf(Contract);
+      });
 
-  it('should emit a new event when a player enter in the lottery', async () => {
-    await expect(
-      LotteryContract.enterLottery({
-        value: entranceFee,
-      })
-    ).to.emit(LotteryContract, 'LotteryEnter');
+      it('VRFCoordinatorV2Mock should be deployed', function () {
+        expect(vrfCoordinatorV2MockContract).instanceOf(Contract);
+      });
+    });
   });
-});
+}
