08195217e627efc0f9cd1ed5dd76aa2f52c392d8
alexboisseau <alex.boisseau79@gmail.com>
2023-05-03 08:54:54 +0200

    test: add unit tests for the enterLottery adn checkUpkeep methods

--- test/Lottery.spec.ts
+++ test/Lottery.spec.ts
@@ -1,22 +1,27 @@
-import { deployments, ethers, network } from 'hardhat';
+import { deployments, ethers, getNamedAccounts, network } from 'hardhat';
 import { developmentChains, networkConfig } from '../helper-hardhat.config';
-import { Contract } from 'ethers';
+import { BigNumberish, Contract } from 'ethers';
 import { expect } from 'chai';
 
 if (developmentChains.includes(network.name)) {
-  describe('Lottery Unit Tests', async function () {
+  describe('Lottery Unit Tests', function () {
+    let deployer: string;
     let lotteryContract: Contract;
     let vrfCoordinatorV2MockContract: Contract;
     let chainId: number;
+    let entranceFee: BigNumberish;
 
     this.beforeEach(async function () {
+      deployer = (await getNamedAccounts()).deployer;
       await deployments.fixture('all'); // Deploy all contracts which have the tag 'all';
-      lotteryContract = await ethers.getContract('Lottery');
+      lotteryContract = await ethers.getContract('Lottery', deployer);
       vrfCoordinatorV2MockContract = await ethers.getContract(
-        'VRFCoordinatorV2Mock'
+        'VRFCoordinatorV2Mock',
+        deployer
       );
 
       chainId = network.config.chainId!;
+      entranceFee = networkConfig[chainId].lotteryEntranceFee;
     });
 
     describe('Deployments', function () {
@@ -45,7 +50,7 @@ if (developmentChains.includes(network.name)) {
         const expectedResult = networkConfig[chainId!].gasLane;
         const lotteryGasLane = await lotteryContract.getGasLane();
 
-        expect(lotteryGasLane).equal(lotteryGasLane);
+        expect(lotteryGasLane).equal(expectedResult);
       });
 
       it(`Lottery subscriptionId should be equal to ${
@@ -81,6 +86,115 @@ if (developmentChains.includes(network.name)) {
         const lotteryInterval = await lotteryContract.getInterval();
 
         expect(lotteryInterval).equal(expectedResult);
+      });
+    });
+
+    describe('Enter Lottery', function () {
+      it("reverts when you don't pay enough", async function () {
+        await expect(lotteryContract.enterLottery()).to.revertedWithCustomError(
+          lotteryContract,
+          'Lottery__NotEnoughETHEntered'
+        );
+      });
+
+      it('player should be registered', async function () {
+        await lotteryContract.enterLottery({
+          value: entranceFee,
+        });
+        const registeredPlayer = await lotteryContract.getPlayer(0);
+
+        expect(registeredPlayer).equal(deployer);
+      });
+
+      it('emits event on enter', async function () {
+        await expect(
+          lotteryContract.enterLottery({
+            value: entranceFee,
+          })
+        ).to.emit(lotteryContract, 'LotteryEnter');
+      });
+
+      it('doesnt allow entrance when lottery is calculating', async function () {
+        // In the smart contract, lottery state is an enum so OPEN = 0 and CALCULATING = 1
+        await lotteryContract.enterLottery({
+          value: entranceFee,
+        });
+        const interval = await lotteryContract.getInterval();
+
+        await network.provider.send('evm_increaseTime', [
+          interval.toNumber() + 1,
+        ]);
+
+        // Will be automatic in the future thanks chainlink automation
+        await lotteryContract.performUpkeep([]);
+
+        await expect(
+          lotteryContract.enterLottery({
+            value: entranceFee,
+          })
+        ).to.revertedWithCustomError(lotteryContract, 'Lottery__NotOpen');
+      });
+    });
+
+    describe('CheckUpkeep', function () {
+      it('returns false lottery is not open', async function () {
+        await lotteryContract.enterLottery({
+          value: entranceFee,
+        });
+        const interval = await lotteryContract.getInterval();
+
+        await network.provider.send('evm_increaseTime', [
+          interval.toNumber() + 1,
+        ]);
+
+        await lotteryContract.performUpkeep([]);
+
+        const { upkeepNeeded } = await lotteryContract.checkUpkeep([]);
+        expect(upkeepNeeded).equal(false);
+      });
+
+      it("returns false if enough time hasn't passed", async function () {
+        await lotteryContract.enterLottery({
+          value: entranceFee,
+        });
+        const interval = await lotteryContract.getInterval();
+
+        await network.provider.send('evm_increaseTime', [
+          interval.toNumber() - 1,
+        ]);
+
+        const { upkeepNeeded } = await lotteryContract.checkUpkeep([]);
+        expect(upkeepNeeded).equal(false);
+      });
+
+      it('returns false if the players array is empty', async function () {
+        const interval = await lotteryContract.getInterval();
+
+        await network.provider.send('evm_increaseTime', [
+          interval.toNumber() + 1,
+        ]);
+        await network.provider.send('evm_mine', []);
+
+        const { upkeepNeeded } = await lotteryContract.checkUpkeep([]);
+        expect(upkeepNeeded).equal(false);
+      });
+
+      it('returns true if enough time has passed, has players and is open', async function () {
+        await lotteryContract.enterLottery({
+          value: entranceFee,
+        });
+        const interval = await lotteryContract.getInterval();
+
+        await network.provider.send('evm_increaseTime', [
+          interval.toNumber() + 1,
+        ]);
+        await network.provider.send('evm_mine', []);
+
+        const players = await lotteryContract.getNumberOfPlayers();
+        console.log('PLAYERS : ', players.toNumber());
+
+        const { upkeepNeeded } = await lotteryContract.checkUpkeep([]);
+        expect(upkeepNeeded).equal(true);
       });
     });
   });
